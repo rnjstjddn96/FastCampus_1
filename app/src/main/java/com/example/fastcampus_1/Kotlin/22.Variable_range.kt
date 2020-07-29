6f6a650c579fcca3ceba87138cadc1d83f6608d9
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-29 15:19:27 +0900

    Kotlin Basic Access_modifier 개념, 실습

--- app/src/main/java/com/example/fastcampus_1/Kotlin/22.Variable_range.kt
+++ app/src/main/java/com/example/fastcampus_1/Kotlin/22.Variable_range.kt
@@ -1,7 +1,34 @@
 package com.example.fastcampus_1.Kotlin
 
-var number100 : Int = 100
+//변수의 접근 범위
+//1.전역변수
+//2.지역변수
 
-fun main(array: Array<String>){
-    
+var number100: Int = 10
+
+fun main(array: Array<String>) {
+
+    val test = Test("권성우")
+    println(number100)
+    println(test.name)
+    test.testFun()
+    println(number100)
+    println(test.name)
+
+}
+
+class Test(var name: String) {
+
+    fun testFun() {
+        var birth : String = "19960225"
+        //함수안에서만 접근 가능한 변수
+        name = "홍길동"
+        number100 = 100
+    }
+
+    fun testFun2(){
+        name = "길동홍"
+        // birth = "1111111" //접근할 수 없다
+
+    }
 }
