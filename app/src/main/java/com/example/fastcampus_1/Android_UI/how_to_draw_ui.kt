2d9a278c54e10381871847d00772e6f792549b61
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-30 11:27:04 +0900

    How to Draw ui

---
+++ app/src/main/java/com/example/fastcampus_1/Android_UI/how_to_draw_ui.kt
@@ -0,0 +1,21 @@
+package com.example.fastcampus_1.Android_UI
+
+//안드로이드에서 화면을 그리는 방법
+//xml을 이용한다
+//xml   -DSL Language
+//      -Domain Specific Language
+//      -안드로이드 UI를 그리기 위해 특화된 언어
+
+// 핸드폰마다 화면 크기가 다 다른데 어떻게 화면을 그려야 할까??
+// 픽셀, dpi, dp 단위
+// - 픽셀 -> 핸드폰에서 빛이 나오 전구 -> 가장 작은 단위
+// - dpi -> dot per inch
+//      - ldpi -> 120 (1인치에 120픽셀)
+//      - mdpi -> 160
+//      - hdpi -> 240
+//      - xhdpi -> 320
+//      - xxhdpi -> 480
+//      - xxhdpi -> 640
+// - dp : density independent Picxel
+//         - 픽셀 독립적인 단위
+
