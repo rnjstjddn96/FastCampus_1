9de84db1ec7542f980363f59ec818ea9d06e17aa
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-31 17:29:04 +0900

    Null Safety/ ? !! / lateninit 개념

--- app/src/main/java/com/example/fastcampus_1/NullSafety.kt
+++ app/src/main/java/com/example/fastcampus_1/NullSafety.kt
@@ -21,7 +21,7 @@ class NullSafety : AppCompatActivity() {
 
         //var number3 = number? + number1
         var number3 = number1?.plus(number)
-        //number1이 null이기 떄문에 plus함수 실행하지 x
+        //number1이 null이기 떄문에 plus함수 실행하지 않는
         Log.d("number", "number3 = " + number3)
 
         //삼항 연산자 -> 엘비스 연산자(?:)
@@ -31,6 +31,7 @@ class NullSafety : AppCompatActivity() {
 
         lateCar = Car(10)
             Log.d("number", "late number : " + lateCar.number)
+        var numbner5 = number1!! + 10 //개발자가 number1이 null이 아님을 보장한다. -> null일 경우 에러 발생
     }
 
     fun plus(a: Int, b: Int?): Int {
