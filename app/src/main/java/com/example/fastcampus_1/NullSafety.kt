f4214c096670675ad0d04ac3ce09d30fb9398d7f
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-31 17:26:22 +0900

    Task 개념, 암시적 intent 실습

---
+++ app/src/main/java/com/example/fastcampus_1/NullSafety.kt
@@ -0,0 +1,44 @@
+package com.example.fastcampus_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+//var lateCar : Car = Car() // 초기화 오류 발생
+lateinit var lateCar: Car //나중에 초기화 하겠다.
+
+class Car(var number : Int) {
+}
+
+class NullSafety : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_null_safety)
+
+        var number: Int = 10
+        var number1: Int? = null
+
+        //var number3 = number? + number1
+        var number3 = number1?.plus(number)
+        //number1이 null이기 떄문에 plus함수 실행하지 x
+        Log.d("number", "number3 = " + number3)
+
+        //삼항 연산자 -> 엘비스 연산자(?:)
+        //Null Safety
+        val number4 = number1 ?: 10
+        Log.d("number", "number4 : " + number4)
+
+        lateCar = Car(10)
+            Log.d("number", "late number : " + lateCar.number)
+    }
+
+    fun plus(a: Int, b: Int?): Int {
+        if (b != null) return a + b
+        else return a
+    }
+
+    fun plus2(a: Int, b: Int?): Int? {
+        return b?.plus(a)
+    }
+}
